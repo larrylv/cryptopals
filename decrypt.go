71807ff4a67b2af80d257ee980e20ef69cb18690
Larry Lv <larrylv1990@gmail.com>
2018-02-07 22:32:41 -0800

    Set 1: Challenge 7 -- Play that funky music again!

--- decrypt.go
+++ decrypt.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/aes"
 	"encoding/base64"
 	"encoding/hex"
 	"fmt"
@@ -42,6 +43,27 @@ func DecryptCipherXoredByRepeatKey(cipher []byte) ([]byte, error) {
 	}
 
 	return decoded, nil
+}
+
+// DecryptAesEcbCipher decrypts the AES (EBC mode) encrypted cipher with the given key
+func DecryptAesEcbCipher(key, cipher []byte) ([]byte, error) {
+	decodedCipher, err := base64.StdEncoding.DecodeString(string(cipher))
+	if err != nil {
+		return nil, fmt.Errorf("DecryptAesEcbCipher: %v", err)
+	}
+
+	cipherBlock, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, fmt.Errorf("DecryptAesEcbCipher: %v", err)
+	}
+
+	size := aes.BlockSize
+	decrypted := make([]byte, len(decodedCipher))
+	for bs, be := 0, size; bs < len(decodedCipher); bs, be = bs+size, be+size {
+		cipherBlock.Decrypt(decrypted[bs:be], decodedCipher[bs:be])
+	}
+
+	return decrypted, nil
 }
 
 // FindRepeatKeySize returns the most possible repeat-key size for the cipher
--- decrypt_test.go
+++ decrypt_test.go
@@ -25,3 +25,24 @@ func TestDecryptCipherXoredByRepeatKey(t *testing.T) {
 		t.Errorf("TestDecryptCipherXoredByRepeatKey: expected it contains %s", "Play that funky music")
 	}
 }
+
+func TestDecryptAesEcbCipher(t *testing.T) {
+	filename := "./data/set_1_challege_7.txt"
+	cipher, err := ioutil.ReadFile(filename)
+	if err != nil {
+		t.Errorf("TestDecryptAesEcbCipher: got an error %v", err)
+		return
+	}
+
+	key := []byte("YELLOW SUBMARINE")
+	decrypted, err := DecryptAesEcbCipher(key, cipher)
+	if err != nil {
+		t.Errorf("TestDecryptAesEcbCipher: got an error %v", err)
+		return
+	}
+	// fmt.Printf("%s\n", decrypted)
+
+	if !bytes.Contains(decrypted, []byte("Play that funky music")) {
+		t.Errorf("TestDecryptAesEcbCipher: expected it contains %s", "Play that funky music")
+	}
+}
