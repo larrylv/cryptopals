9e6143ab4a597bc90f6382845d6fcec26d33c13d
Larry Lv <larrylv1990@gmail.com>
2018-02-07 20:11:12 -0800

    Set 1: Chanllenge 5

--- util_test.go
+++ util_test.go
@@ -34,6 +34,19 @@ func TestXor(t *testing.T) {
 	}
 }
 
+func TestRepeatKeyXor(t *testing.T) {
+	str := "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
+	key := `ICE`
+	expectedEncoded := `0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f`
+
+	encoded, err := RepeatKeyXor([]byte(key), []byte(str))
+	if err != nil {
+		t.Fatalf("RepeatKeyXor: got an error %v\n", err)
+	} else if bytes.Compare([]byte(expectedEncoded), encoded) != 0 {
+		t.Fatalf("RepeatKeyXor: expected %s, got %s\n", expectedEncoded, encoded)
+	}
+}
+
 func TestFindSingleKeyForXorCipher(t *testing.T) {
 	a := `1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736`
 	decodedA, _ := hex.DecodeString(a)
