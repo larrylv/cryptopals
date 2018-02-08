ad5b86282e9f566d35844afd1fd19b12bcc93d34
Larry Lv <larrylv1990@gmail.com>
2018-02-07 22:08:04 -0800

    Remove useless FindRepeatKeySize test

--- decrypt_test.go
+++ decrypt_test.go
@@ -25,12 +25,3 @@ func TestDecryptCipherXoredByRepeatKey(t *testing.T) {
 		t.Errorf("TestDecryptCipherXoredByRepeatKey: expected it contains %s", "Play that funky music")
 	}
 }
-
-func TestFindRepeatKeySize(t *testing.T) {
-	cipher := `HUIfTQsPAh9PE048GmllH0kcDk4TAQsHThsBFkU2AB4BSWQgVB0dQzNTTmVS`
-	keySize := FindRepeatKeySize([]byte(cipher))
-
-	if keySize != 3 {
-		t.Errorf("TestFindRepeatKeySize: expected 3, got %d\n", keySize)
-	}
-}
