8413f3b7fffe17f86d971708683abaef2401eeba
Larry Lv <larrylv1990@gmail.com>
2018-02-08 22:04:21 -0800

    Refactor: create AesEcbCipher struct

--- aes.go
+++ aes.go
@@ -6,34 +6,51 @@ import (
 	"fmt"
 )
 
-// DecryptAesEcbCipher decrypts the AES (EBC mode) encrypted cipher with the given key
-func DecryptAesEcbCipher(key, cipher []byte) ([]byte, error) {
-	decodedCipher, err := base64.StdEncoding.DecodeString(string(cipher))
+// type aesCipher interface {
+// 	Encrypt(plaintext []byte) ([]byte, error)
+// 	Decrypt(ciphertext []byte) ([]byte, error)
+// }
+
+// AesEcbCipher is just an AES ECB mode cipher...
+type AesEcbCipher struct {
+	key []byte
+}
+
+// BlockSize is the AES block size in bytes.
+const BlockSize = 16
+
+// NewAesEcbCipher returns an AES-128 ECB mode cipher
+func NewAesEcbCipher(key []byte) *AesEcbCipher {
+	return &AesEcbCipher{key: key}
+}
+
+// Decrypt of AesEcbCipher implements Decrypt function of aesCipher interface
+func (cipher *AesEcbCipher) Decrypt(ciphertext []byte) ([]byte, error) {
+	decodedCipher, err := base64.StdEncoding.DecodeString(string(ciphertext))
 	if err != nil {
-		return nil, fmt.Errorf("DecryptAesEcbCipher: %v", err)
+		return nil, fmt.Errorf("AesEcbCipher.Decrypt: %v", err)
 	}
 
-	cipherBlock, err := aes.NewCipher(key)
+	cipherBlock, err := aes.NewCipher(cipher.key)
 	if err != nil {
-		return nil, fmt.Errorf("DecryptAesEcbCipher: %v", err)
+		return nil, fmt.Errorf("AesEcbCipher.Decrypt: %v", err)
 	}
 
-	size := aes.BlockSize
 	decrypted := make([]byte, len(decodedCipher))
-	for bs, be := 0, size; bs < len(decodedCipher); bs, be = bs+size, be+size {
+	for bs, be := 0, BlockSize; bs < len(decodedCipher); bs, be = bs+BlockSize, be+BlockSize {
 		cipherBlock.Decrypt(decrypted[bs:be], decodedCipher[bs:be])
 	}
 
 	return decrypted, nil
 }
 
-// DetectAesInEcbMode returns if the cipher is encrypted with AES in ECB mode
-func DetectAesInEcbMode(cipher []byte) bool {
+// IsEncryptedWithAesEcbMode returns if the ciphertext is encrypted with AES in ECB mode
+func IsEncryptedWithAesEcbMode(ciphertext []byte) bool {
 	m := make(map[string]bool)
-	blockSize := aes.BlockSize
+	blockSize := BlockSize
 
-	for i := 0; i+1 <= len(cipher)/blockSize; i++ {
-		block := cipher[i*blockSize : (i+1)*blockSize]
+	for i := 0; i+1 <= len(ciphertext)/blockSize; i++ {
+		block := ciphertext[i*blockSize : (i+1)*blockSize]
 		if m[string(block)] {
 			return true
 		}
--- aes_test.go
+++ aes_test.go
@@ -11,14 +11,13 @@ import (
 
 func TestDecryptAesEcbCipher(t *testing.T) {
 	filename := "./data/set_1_challege_7.txt"
-	cipher, err := ioutil.ReadFile(filename)
+	ciphertext, err := ioutil.ReadFile(filename)
 	if err != nil {
 		t.Errorf("TestDecryptAesEcbCipher: got an error %v", err)
 		return
 	}
 
-	key := []byte("YELLOW SUBMARINE")
-	decrypted, err := DecryptAesEcbCipher(key, cipher)
+	decrypted, err := NewAesEcbCipher([]byte("YELLOW SUBMARINE")).Decrypt(ciphertext)
 	if err != nil {
 		t.Errorf("TestDecryptAesEcbCipher: got an error %v", err)
 		return
@@ -30,11 +29,11 @@ func TestDecryptAesEcbCipher(t *testing.T) {
 	}
 }
 
-func TestDetectAesInEcbMode(t *testing.T) {
+func TestIsEncryptedWithAesEcbMode(t *testing.T) {
 	filename := "./data/set_1_challege_8.txt"
 	file, err := os.Open(filename)
 	if err != nil {
-		t.Errorf("TestDetectAesInEcbMode: got an error %v", err)
+		t.Errorf("TestIsEncryptedWithAesEcbMode: got an error %v", err)
 		return
 	}
 	defer file.Close()
@@ -50,11 +49,11 @@ func TestDetectAesInEcbMode(t *testing.T) {
 
 		curDecoded, err := hex.DecodeString(string(curLine))
 		if err != nil {
-			t.Errorf("TestDetectAesInEcbMode: got an error %v", err)
+			t.Errorf("TestIsEncryptedWithAesEcbMode: got an error %v", err)
 			return
 		}
 
-		if DetectAesInEcbMode(curDecoded) {
+		if IsEncryptedWithAesEcbMode(curDecoded) {
 			result = make([]byte, len(curLine))
 			copy(result, curLine)
 			break
@@ -63,6 +62,6 @@ func TestDetectAesInEcbMode(t *testing.T) {
 
 	expected := `d880619740a8a19b7840a8a31c810a3d08649af70dc06f4fd5d2d69c744cd283e2dd052f6b641dbf9d11b0348542bb5708649af70dc06f4fd5d2d69c744cd2839475c9dfdbc1d46597949d9c7e82bf5a08649af70dc06f4fd5d2d69c744cd28397a93eab8d6aecd566489154789a6b0308649af70dc06f4fd5d2d69c744cd283d403180c98c8f6db1f2a3f9c4040deb0ab51b29933f2c123c58386b06fba186a`
 	if bytes.Compare(result, []byte(expected)) != 0 {
-		t.Errorf("TestDetectAesInEcbMode: expected %s, got %s\n", expected, result)
+		t.Errorf("TestIsEncryptedWithAesEcbMode: expected %s, got %s\n", expected, result)
 	}
 }
