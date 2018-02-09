c784b3fb6224143c50c66c82c4a6c61aa2a96cf9
Larry Lv <larrylv1990@gmail.com>
2018-02-08 21:26:42 -0800

    Refactor: Rename crypto to aes

--- aes.go
+++ aes.go
@@ -5,7 +5,6 @@ import (
 	"crypto/aes"
 	"encoding/base64"
 	"fmt"
-	"math"
 )
 
 // DecryptAesEcbCipher decrypts the AES (EBC mode) encrypted cipher with the given key
@@ -41,34 +40,4 @@ func Pkcs7Padding(plaintext []byte, blockSize int) ([]byte, error) {
 	cipher := append(plaintext, bytes.Repeat([]byte{paddedByte}, paddedCnt)...)
 
 	return cipher, nil
-}
-
-// FindRepeatKeySize returns the most possible repeat-key size for the cipher
-func FindRepeatKeySize(cipher []byte) int {
-	keySize := 0
-	minDistance := math.Inf(1)
-
-	for i := 2; i <= 64; i++ {
-		// exceeds the cipher length
-		if 2*i > len(cipher) {
-			break
-		}
-
-		distance := 0.0
-		cnt := 0
-
-		for j := 0; j < 10 && (j+2)*i <= len(cipher); j++ {
-			distance += float64(HammingDistance(cipher[j*i:(j+1)*i], cipher[(j+1)*i:(j+2)*i]))
-			cnt++
-		}
-
-		distance /= float64(cnt * i)
-
-		if distance < minDistance {
-			keySize = i
-			minDistance = distance
-		}
-	}
-
-	return keySize
 }
--- repeat_key.go
+++ repeat_key.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"math"
 )
 
 // RepeatKeyXor encrypts the string using repeat-key XOR
@@ -30,7 +31,7 @@ func DecryptRepeatKeyXor(cipher []byte) ([]byte, error) {
 		return nil, fmt.Errorf("DecryptRepeatKeyXor: %v", err)
 	}
 
-	keySize := FindRepeatKeySize(decodedCipher)
+	keySize := findRepeatKeySize(decodedCipher)
 	var key []byte
 
 	for i := 0; i < keySize; i++ {
@@ -57,4 +58,34 @@ func DecryptRepeatKeyXor(cipher []byte) ([]byte, error) {
 	}
 
 	return decoded, nil
+}
+
+// findRepeatKeySize returns the most possible repeat-key size for the cipher
+func findRepeatKeySize(cipher []byte) int {
+	keySize := 0
+	minDistance := math.Inf(1)
+
+	for i := 2; i <= 64; i++ {
+		// exceeds the cipher length
+		if 2*i > len(cipher) {
+			break
+		}
+
+		distance := 0.0
+		cnt := 0
+
+		for j := 0; j < 10 && (j+2)*i <= len(cipher); j++ {
+			distance += float64(HammingDistance(cipher[j*i:(j+1)*i], cipher[(j+1)*i:(j+2)*i]))
+			cnt++
+		}
+
+		distance /= float64(cnt * i)
+
+		if distance < minDistance {
+			keySize = i
+			minDistance = distance
+		}
+	}
+
+	return keySize
 }
